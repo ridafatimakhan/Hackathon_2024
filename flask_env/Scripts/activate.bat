4d71ad7633542735ba0e0cc6278db018bedfcc70
Helena Carmen Udu <helena.udu@taltech.ee>
2024-11-29 21:19:53 +0200

    page_index branch created

--- flask_env/Scripts/activate.bat
+++ flask_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Hp\OneDrive\Documents\GitHub\Hackathon_2024\flask_env
+set VIRTUAL_ENV=C:\Users\helen\VisualStudio_Projects\hackathon2024\Hackathon_2024\flask_env
 
 if not defined PROMPT set PROMPT=$P$G
 
